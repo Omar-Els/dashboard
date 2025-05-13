a4b89621a672f1ee114825c7fbfbfd5e889b3475
omar <oe21741@gmail.com>
2025-05-13 05:20:22 +0300

    fix setup for GitHub Pages deployment

---
+++ App.jsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+import { createTheme, styled, ThemeProvider } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+
+import CssBaseline from "@mui/material/CssBaseline";
+import Typography from "@mui/material/Typography";
+
+import TopBar from "./components/TopBar";
+import SideBar from "./components/SidrBar";
+import { getDesignTokens } from "./theme";
+
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+
+export default function MiniDrawer() {
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const [mode, setMode] = React.useState ("dark");
+  const theme = React.useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
+  return (
+    <ThemeProvider theme={theme}>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+
+        <TopBar
+          open={open}
+          handleDrawerOpen={handleDrawerOpen}
+          setMode={setMode}
+        />
+        <SideBar open={open} handleDrawerClose={handleDrawerClose} />
+
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <DrawerHeader />
+          <Typography sx={{ marginBottom: 2 }}>
+            Lorem ipsum dolor sit amet
+          </Typography>
+        </Box>
+      </Box>
+    </ThemeProvider>
+  );
+}
